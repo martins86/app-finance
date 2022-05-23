31fac219e1b459fab1598ce587f7a7f1ecff527e
Thiago Martins de Freitas <51454606+thiagodf-cit@users.noreply.github.com>
2022-05-23 17:25:09 -0300

    resolvendo erro sonar regexp

--- src/utils/RegEx/index.tsx
+++ src/utils/RegEx/index.tsx
@@ -1,13 +1,9 @@
 /**
  * Valida se é um email padrão ex: email@email.com.
  */
-export const validateEmail = new RegExp(
-  '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
-);
+export const validateEmail = new RegExp('^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$');
 
 /**
  * Valida se é uma senha forte.
  */
-export const validateStrongPassword = new RegExp(
-  '^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$'
-);
+export const validateStrongPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
