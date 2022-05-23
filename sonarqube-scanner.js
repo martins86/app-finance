eaad973e0439c72c03e76231523c318232e13b3e
Thiago Martins de Freitas <51454606+thiagodf-cit@users.noreply.github.com>
2022-05-23 15:45:16 -0300

    configurando sonar

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -2,19 +2,20 @@ const scanner = require('sonarqube-scanner');
 scanner(
   {
     serverUrl: 'http://localhost:9000',
-    login: 'thiagodf',
-    password: 'Thiagodf@2022!',
+    login: 'admin',
+    password: 'admin',
     options: {
+      'sonar.login': 'admin',
+      'sonar.password': 'admin',
+      'sonar.sourceEncoding': 'utf8',
       'sonar.sources': './src',
-      'sonar.exclusions': '**/__tests__/**,**/*.ts,**/*.js',
-      'sonar.tests': './src',
-      'sonar.test.inclusions': '**/*.test.tsx,**/*.test.ts',
-      'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
-      'sonar.testExecutionReportPaths': 'test-report.xml',
-      'sonar.exclusions': '',
       'sonar.tests': './src/__tests__',
       'sonar.test.inclusions':
         './src/__tests__/**/*.test.tsx,./src/__tests__/**/*.test.ts',
+      'sonar.exclusions': '**/__tests__/**,**/*.ts,**/*.js',
+      'sonar.typescript.tsconfigPath': 'tsconfig.json',
+      'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
+      'sonar.testExecutionReportPaths': 'test-report.xml',
       'sonar.eslint.reportPaths': 'eslint-report.json',
     },
   },
