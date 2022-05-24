f25b5b6d9f2994bb65faceef79c518796658d3c1
martins86@gmail.com <martins86@gmail.com>
2022-05-23 23:18:41 -0300

    ajuste regex

--- src/utils/RegEx/index.tsx
+++ src/utils/RegEx/index.tsx
@@ -1,9 +1,11 @@
 /**
  * Valida se é um email padrão ex: email@email.com.
  */
-export const validateEmail = new RegExp('^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$');
+const regFormatEmail = '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$';
+export const validateEmail = new RegExp(regFormatEmail);
 
 /**
  * Valida se é uma senha forte.
  */
-export const validateStrongPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
+const regStrongPw = '^(?=.*?[A-Za-z])(?=.*?\\d).{6,}$';
+export const validateStrongPassword = new RegExp(regStrongPw);
