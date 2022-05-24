c84b5dc92d8460c89645e0b55426e7deea2088bb
martins86@gmail.com <martins86@gmail.com>
2022-05-24 10:05:04 -0300

    ajuste no coverage

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -12,7 +12,8 @@ scanner(
       'sonar.tests': './src/__tests__',
       'sonar.test.inclusions':
         './src/__tests__/**/*.test.tsx,./src/__tests__/**/*.test.ts',
-      'sonar.exclusions': '**/__tests__/**,**/*.ts,**/*.js',
+      'sonar.exclusions':
+        '**/__tests__/**,**/*.ts,**/*.js,src/index.tsx,src/utils/**',
       'sonar.typescript.tsconfigPath': 'tsconfig.json',
       'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
       'sonar.testExecutionReportPaths': 'test-report.xml',
